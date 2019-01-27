b241158b688f9d78952a3b4d463d8ce5ce4d2862
Hiroshi Ueki <hirocueki@gmail.com>
2019-01-27 22:16:28 +0900

    ログアウトの実装￥

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,6 @@
+class Users::SessionsController < ApplicationController
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       omniauth_callbacks: "users/omniauth_callbacks"
   }
+  as :user do
+    delete '/users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
 
   resources :answer_boards
   resources :question_boards do
