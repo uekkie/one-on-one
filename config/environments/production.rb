c100f199ff98a78b78eb326723ec30ece52e20b8
Hiroshi Ueki <hirocueki@gmail.com>
2019-02-12 16:20:37 +0900

    mailgunの設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,17 +68,13 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'one-on-one-rails.herokuapp.com',
-    :authentication => :plain,
-  }
 
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain:  ENV['MAILGUN_DOMAIN'],
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
