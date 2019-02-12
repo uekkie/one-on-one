d73a5690edfc72cb3657a3a34cc3eb6756365e82
Hiroshi Ueki <hirocueki@gmail.com>
2019-02-12 15:47:48 +0900

    fix mailgun configure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'one-on-one.herokuapp.com',
+    :domain         => 'one-on-one-rails.herokuapp.com',
     :authentication => :plain,
   }
 
