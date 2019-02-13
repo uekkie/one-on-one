a76995a71a35dce91238cd3da54ba1c1acced1e2
Hiroshi Ueki <hirocueki@gmail.com>
2019-02-13 09:53:14 +0900

    早めのリターン

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,14 @@ class User < ApplicationRecord
   def self.find_for_google(auth)
     user = User.find_by(email: auth.info.email)
 
-    unless user
-      user = User.create( email:    auth.info.email,
-                          name:     auth.info.name,
-                          provider: auth.provider,
-                          uid:      auth.uid,
-                          token:    auth.credentials.token,
-                          password: Devise.friendly_token[0, 20],
-                          meta:     auth.to_yaml
-                        )
-    end
-    user
+    return user if user
+    user = User.create( email:    auth.info.email,
+                        name:     auth.info.name,
+                        provider: auth.provider,
+                        uid:      auth.uid,
+                        token:    auth.credentials.token,
+                        password: Devise.friendly_token[0, 20],
+                        meta:     auth.to_yaml
+                      )
   end
 end
