b792632ebd876d8e184ba0243ab5bfe453b8413f
Hiroshi Ueki <hirocueki@gmail.com>
2019-02-14 22:47:16 +0900

    default値を削除。空文字は許可しない

--- db/migrate/20190123132431_create_invites.rb
+++ db/migrate/20190123132431_create_invites.rb
@@ -3,10 +3,10 @@ class CreateInvites < ActiveRecord::Migration[5.2]
     create_table :invites do |t|
       t.references :user, foreign_key: true
       t.references :question_board, foreign_key: true
-      t.string :name, null: false, default: ""
-      t.string :email, null: false, default: ""
-      t.string :token, null: false, default: ""
-      
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :token, null: false
+
       t.timestamps
     end
     add_index :invites, :token, unique: true
