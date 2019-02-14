4dd72c5930d3405eaf935042faab3bce9476693e
Hiroshi Ueki <hirocueki@gmail.com>
2019-02-14 14:18:45 +0900

    NotNull制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_150146) do
+ActiveRecord::Schema.define(version: 2019_02_14_051612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answer_boards", force: :cascade do |t|
-    t.bigint "invite_id"
+    t.bigint "invite_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invite_id"], name: "index_answer_boards_on_invite_id"
