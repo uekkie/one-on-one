4f24f08c77131eff18d16fb332a1b9487c05fde4
Hiroshi Ueki <hirocueki@gmail.com>
2019-02-20 22:12:10 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,12 @@ module OneOnOne
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
     config.generators do |g|
-      g.test_frameworkv :rspec,
+      g.test_framework :rspec,
       fixtures: false,
       view_specs: false,
       helper_specs: false,
-      routing_specs: false,
+      routing_specs: false
     end
   end
 end
